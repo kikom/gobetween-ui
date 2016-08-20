f2992ff2fc42b78739470489a94863b2d5175747
Gene Ponomarenko <kikomdev@gmail.com>
2016-08-21 02:25:36 +0300

    Initial preparations. Setup webpack to work with angular2. Created dummy angular2 app.

---
+++ src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+import '../../public/css/styles.css';
+@Component({
+    selector: 'my-app',
+    templateUrl: 'templates/app.component.html'
+})
+export class AppComponent {
+}
